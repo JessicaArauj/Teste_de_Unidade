b047fb0ae1ee73a6852d4d5f4820e9d1f287c91d
Jessica Teixeira Araujo <jessicateixeiraaraujo@gmail.com>
2023-04-26 16:13:25 -0300

    script utilizando Jest para técnica TDD

--- TDD_Jest/codigo.test.js
+++ TDD_Jest/codigo.test.js
@@ -1,10 +1,22 @@
-function soma(a, b) {
-    return a + b;
-  }
-  
+const soma = require('./codigo');
+
   describe('soma', () => {
-    it('deve retornar a soma de dois números', () => {
-      expect(soma(1, 2)).toBe(3);
+
+    beforeAll(() => {
+      console.log('Antes do teste')
+    })
+
+    beforeEach(() => {
+      console.log('Depois do teste')
     });
+
+    it('deve retornar a soma de dois números', () => {
+      const a = Math.floor(Math.random() * 100);
+      const b = Math.floor(Math.random() * 100);
+      expect(soma(a, b)).toBe(a + b);
+    });
+
+
   });
+  
   
